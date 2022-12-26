70a5f279f512a40cd2cf700a4d150d4ba0284ed8
Florian Reisinger <reisi007@gmail.com>
2022-12-26 18:05:43 +0100

    Add favicon

--- components/GalleryPage.tsx
+++ components/GalleryPage.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import React, { ReactNode } from 'react';
+import Head from 'next/head';
 import { CONTAIINER_CLASSES } from './images-next/utils/Css';
 import { FooterContent } from './images-next/page/FooterContent';
 
@@ -12,6 +13,13 @@ export function GalleryPage({
 }: GalleryPageProps) {
   return (
     <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      </Head>
       <header>
         <h1>{title}</h1>
       </header>
