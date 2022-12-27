fd9080ffabb735c2b5f777aff46561cc5e2e8ff7
Florian Reisinger <reisi007@gmail.com>
2022-12-27 16:57:14 +0100

    Add meta tags

--- components/GalleryPage.tsx
+++ components/GalleryPage.tsx
@@ -6,14 +6,24 @@ import { FooterContent } from './images-next/page/FooterContent';
 
 export type GalleryPageProps = { children: ReactNode, title: string, className?: string };
 
+export type HeaderProps = { title: string, description?: string, keywords?: Array<string> };
+
 export function GalleryPage({
   children,
   className,
   title,
-}: GalleryPageProps) {
+  keywords,
+  description,
+}: GalleryPageProps & HeaderProps) {
+  const keywordString = keywords?.join(',');
   return (
     <>
       <Head>
+        <title>{title}</title>
+        <meta charSet="UTF-8" />
+        <meta name="description" content={description} />
+        {keywordString !== undefined && keywordString.length > 0 && <meta name="keywords" content={keywordString} />}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
