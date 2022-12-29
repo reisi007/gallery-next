045476d976a4d8e7b7efe429a761e91152d98835
Florian Reisinger <reisi007@gmail.com>
2022-12-29 08:29:05 +0100

    Adapt to improvements made in the main website

--- components/GalleryPage.tsx
+++ components/GalleryPage.tsx
@@ -1,42 +1,25 @@
-import classNames from 'classnames';
-import React, { ReactNode } from 'react';
-import Head from 'next/head';
-import { FooterContent } from './images-next/page/FooterContent';
-import styles from './images-next/utils/Utils.module.css';
+import React from 'react';
 
-export type GalleryPageProps = { children: ReactNode, title: string, className?: string };
+import { PathEntry } from './images-next/page/NavMenu';
+import { BasePage, BasePageProps } from './images-next/page/BasePage';
+import { HeaderProps } from './images-next/page/Header';
 
-export type HeaderProps = { title: string, description?: string, keywords?: Array<string> };
+const PATHS: { [key: string]: PathEntry } = {
+  categories: {
+    title: 'Alle Kategorien',
+    important: true,
+  },
+  tags: {
+    title: 'Alle Tags',
+    important: true,
+  },
+};
 
 export function GalleryPage({
   children,
-  className,
-  title,
-  keywords,
-  description,
-}: GalleryPageProps & HeaderProps) {
-  const keywordString = keywords?.join(',');
+  ...props
+}: BasePageProps & Omit<HeaderProps, 'menuItems'>) {
   return (
-    <>
-      <Head>
-        <title>{title}</title>
-        <meta charSet="UTF-8" />
-        <meta name="description" content={description} />
-        {keywordString !== undefined && keywordString.length > 0 && <meta name="keywords" content={keywordString} />}
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-      </Head>
-      <header className="my-4">
-        <h1>{title}</h1>
-      </header>
-      <main className={classNames(styles.container, className)}>
-        {children}
-      </main>
-      <FooterContent />
-    </>
+    <BasePage menuItems={PATHS} {...props}>{children}</BasePage>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,26 @@
-import Link from 'next/link';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import { GalleryPage } from '../components/GalleryPage';
+import { loadImageDetails } from '../components/static/loadJson';
 
-export default function Index() {
+export default function Index({ numberOfImages }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <GalleryPage title="Übersicht">
-      <ul>
-        <li><Link href="/categories">Alle Kategorien</Link></li>
-        <li><Link href="/tags">Alle Tags</Link></li>
-      </ul>
+      <h3>Herzlich Willkommen auf meiner Webseite</h3>
+      <p className="text-center">
+        Auf dieser Webseite kannst du
+        {' '}
+        <big className="font-logo text-primary">{numberOfImages}</big>
+        {' '}
+        meiner Bilder sehen. Klicke dich einfach durch!
+      </p>
     </GalleryPage>
   );
 }
+
+export const getStaticProps: GetStaticProps<{ numberOfImages: number }> = async () => {
+  const numberOfImages = Object.keys(await loadImageDetails()).length;
+
+  return {
+    props: { numberOfImages },
+  };
+};
