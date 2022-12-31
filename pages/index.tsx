e78afc917dcf3d61896a123e10098f29f7b0d411
Florian Reisinger <reisi007@gmail.com>
2022-12-31 13:04:07 +0100

    Add google site verification

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import Head from 'next/head';
 import { GalleryPage } from '../components/GalleryPage';
 import { loadImageDetails } from '../components/static/loadJson';
 
 export default function Index({ numberOfImages }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <GalleryPage title="Übersicht">
+      <Head>
+        <meta name="google-site-verification" content="WzDto29AqxoBHzInsq0NZnNhkrP26qqIsFPQmtBiCKw" />
+      </Head>
       <h3>Herzlich Willkommen auf meiner Webseite</h3>
       <p className="text-center">
         Auf dieser Webseite kannst du
