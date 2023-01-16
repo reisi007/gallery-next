4d7676e002db185acdf3e2254af51a2eb170b2e7
Florian Reisinger <reisi007@gmail.com>
2023-01-16 17:09:37 +0100

    Improvements from portfolio (MDX)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    pageExtensions: ['tsx', 'mdx'],
 }
 
-module.exports = nextConfig
+const withMDX = require('@next/mdx')({
+    extension: /\.mdx?$/,
+    options: {
+        // If you use remark-gfm, you'll need to use next.config.mjs
+        // as the package is ESM only
+        // https://github.com/remarkjs/remark-gfm#install
+        remarkPlugins: [],
+        rehypePlugins: [],
+        // If you use `MDXProvider`, uncomment the following line.
+        // providerImportSource: "@mdx-js/react",
+    },
+})
+
+module.exports = withMDX(nextConfig)
