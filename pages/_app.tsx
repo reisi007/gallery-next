ef3a28b622e0f4d0064e004bc5ebbe7c4c0039f8
Florian Reisinger <reisi007@gmail.com>
2023-01-18 07:22:39 +0100

    Improvements from portfolio

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-
-import { init, push } from '@socialgouv/matomo-next';
-
-import { useEffect } from 'react';
 import { CookieConsentContext } from '../components/images-next/cookies/Cookies';
 import { ConsentBanner } from '../components/images-next/cookies/ConsentBanner';
+import { Matomo } from '../components/images-next/cookies/Matomo';
 
-const MATOMO_URL = 'https://analytics.reisinger.pictures/';
-const MATOMO_SITE_ID = '9';
 export default function App({
   Component,
   pageProps,
 }: AppProps) {
-  useEffect(() => {
-    init({
-      url: MATOMO_URL,
-      siteId: MATOMO_SITE_ID,
-    });
-    push(['requireCookieConsent']);
-  }, []);
   return (
     <CookieConsentContext>
+      <Matomo siteId="9" />
       <ConsentBanner />
       <Component {...pageProps} />
     </CookieConsentContext>
